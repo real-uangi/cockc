a578ffcaaefc1d45a26b0fe4aff212b14f57e549
shiyoucai <xuhangqi7855@foxmail.com>
2023-07-31 14:55:13 +0800

    runner with http serve

--- runner/runner.go
+++ runner/runner.go
@@ -6,13 +6,18 @@ package runner
 import (
 	"github.com/real-uangi/cockc/client"
 	"github.com/real-uangi/cockc/common/datasource"
+	"github.com/real-uangi/cockc/common/plog"
 	"github.com/real-uangi/cockc/common/rdb"
 	"github.com/real-uangi/cockc/common/snowflake"
 	"github.com/real-uangi/cockc/config"
+	"net/http"
 )
 
+var logger = plog.New("runner")
+
 type CockRunner struct {
-	cockClient client.CockClientService
+	cockClient       client.CockClientService
+	httpServerEnable bool
 }
 
 func Prepare() *CockRunner {
@@ -36,9 +41,31 @@ func (r *CockRunner) InitDatasource() {
 	datasource.InitDataSource()
 }
 
-func (r *CockRunner) Init() {
+func (r *CockRunner) HttpHandle(pattern string, handler http.Handler) {
+	if !r.httpServerEnable {
+		r.httpServerEnable = true
+	}
+	http.Handle(pattern, handler)
+}
 
+func (r *CockRunner) HttpHandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	if !r.httpServerEnable {
+		r.httpServerEnable = true
+	}
+	http.HandleFunc(pattern, handler)
+}
+
+func (r *CockRunner) Init() {
+	if r.httpServerEnable {
+		go func() {
+			port := config.GetPropertiesRO().Server.Http.Port
+			logger.Info("http server listen on port " + port)
+			err := http.ListenAndServe(":"+port, nil)
+			if err != nil {
+				logger.Error(err.Error())
+			}
+		}()
+	}
 	r.cockClient.StartHeartbeat()
 	r.cockClient.Online()
-
 }
--- test/runner_test.go
+++ test/runner_test.go
@@ -4,7 +4,9 @@
 package test
 
 import (
+	"github.com/real-uangi/cockc/common/api"
 	"github.com/real-uangi/cockc/runner"
+	"net/http"
 	"testing"
 	"time"
 )
@@ -13,6 +15,16 @@ func TestRunner(t *testing.T) {
 	r := runner.Prepare()
 	r.EnableRedisAndSnowflake()
 	r.InitDatasource()
+
+	r.HttpHandleFunc("/hello", handleHttp)
+
 	r.Init()
-	time.Sleep(time.Minute)
+	time.Sleep(time.Hour)
+}
+
+func handleHttp(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write(api.Success("hi!").JsonBytes())
+	if err != nil {
+		return
+	}
 }
